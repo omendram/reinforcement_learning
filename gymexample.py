1a23c094faab8c5e0749c61ce3a7ff787f9a5426
Omendra Manhar <omendra.manhar@gmail.com>
2018-11-26 16:50:26 +0100

    Update gymexample.py

--- gymexample.py
+++ gymexample.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np
 import random
+import pickle
 
 slow = True
 gym.envs.register(
@@ -12,29 +13,33 @@ env = gym.make("MountainCar-v0")
 observation = env.reset()
 
 
-class Sarsa:
+class Sarsa(object):
     def __init__(self):
         self.states = np.zeros((180, 140), dtype=float)
         self.actions = [0, 1, 2]
         self.reward = -1
         self.discount_factor = 0.9
-        self.alpha = 0.95
-        self.td_parameter = 1
+        self.alpha = 0.9
+        self.td_parameter = 0.6
         self.e_values = np.zeros((180, 140, 3), dtype=float)
         self.q_table = np.random.randn(180, 140, 3)
 
     def epochs(self, iters):
         for x in range(iters):
             new_observation = env.reset()
+
             self.learn(new_observation)
+        with open("checkpoint.q", "wb") as check:
+            pickle.dump(self.q_table, check)
+
         self.show_results()
 
     def show_results(self):
         print("EXECUTION")
         new_observation = env.reset()
-        env.render()
 
         while True:
+            env.render()
             position = int((round(new_observation[0], 2) + 1.2) * 10)
             velocity = int((round(new_observation[1], 3) + 0.07) * 100)
             a = np.argmax(self.q_table[position, velocity])
@@ -66,12 +71,8 @@ class Sarsa:
             delta = -1 + self.discount_factor * q_next - q
             self.e_values[position, velocity, a] += 1
 
-            for i in range(180):
-                for j in range(140):
-                    for k in range(3):
-                        self.q_table[i, j, k] += self.alpha * delta * self.e_values[i, j, k]
-                        self.e_values[i, j, k] *= self.discount_factor * self.td_parameter
-
+            self.q_table += self.alpha * delta * self.e_values
+            self.e_values *= self.discount_factor * self.td_parameter
             position = position_next
             velocity = velocity_next
             a = a_next
